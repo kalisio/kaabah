c6027cbff77d8bc172422c71c48195329213022c
Luc Claustres <luc.claustres@orange.fr>
2018-03-16 14:53:35 +0100

    Try to fix travis build by adding commands, ssh key

---
+++ commands.tf
@@ -0,0 +1,42 @@
+# Create a null resource is a trick to ensure the commands are send on each apply
+# Otherwise they are only run when provisioning new resources
+resource "null_resource" "deploy" {
+	count = "${var.instance_count}"
+
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/root/docker-compose.yml"
+    connection {
+	    type = "ssh"
+	    user = "root"
+	    private_key = "${file("ssh.pem")}"
+	    host = "${element(scaleway_ip.instance_ip.*.ip, count.index)}"
+	    timeout = "30s"
+	  }
+  }
+  provisioner "file" {
+    source      = "traefik.toml"
+    destination = "/root/traefik.toml"
+    connection {
+	    type = "ssh"
+	    user = "root"
+	    private_key = "${file("ssh.pem")}"
+	    host = "${element(scaleway_ip.instance_ip.*.ip, count.index)}"
+	    timeout = "30s"
+	  }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+    	"docker network create traefik",
+      "docker-compose up -d"
+    ]
+    connection {
+	    type = "ssh"
+	    user = "root"
+	    private_key = "${file("ssh.pem")}"
+	    host = "${element(scaleway_ip.instance_ip.*.ip, count.index)}"
+	    timeout = "30s"
+	  }
+  }
+}
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 terraform init
-#terraform apply -auto-approve
+terraform apply -auto-approve

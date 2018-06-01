b09cdefb92bba54afb94fee9d8648c7d472fd09c
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-02 00:46:51 +0200

    Updated script

--- utilities/terracheck.sh
+++ utilities/terracheck.sh
@@ -5,6 +5,7 @@ head -n -1 workspaces | while read line
 do
   if [[ $line != *"default"* ]]
   then
+    echo "Processing $line workspace"
     terraform workspace select $line
     terraform apply -var-file "workspaces/$line.tfvars"
   fi

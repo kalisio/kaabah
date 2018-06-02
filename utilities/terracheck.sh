10e81ffcede9de5b9b683c4a1ec513746809b2c0
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-02 18:25:02 +0200

    Updated terracheck

--- utilities/terracheck.sh
+++ utilities/terracheck.sh
@@ -2,9 +2,15 @@
 
 # Initilalize terraform
 terraform init -backend-config="backend.config"
+if [ $? -eq 0 ]; then
+  exit 1
+fi
 
 # List the workspaces
 terraform workspace list > workspaces
+if [ $? -eq 0 ]; then
+  exit 1
+fi
 
 # Check the workspaces
 head -n -1 workspaces | while read line

7b2bcd531e1203c59849938b907cb84eb9da0e63
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-02 18:05:30 +0200

    Updated terracheck script

--- utilities/terracheck.sh
+++ utilities/terracheck.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Initilalize terraform
+terraform init -backend-config="backend.config"
+
+# List the workspaces
 terraform workspace list > workspaces
 
+# Check the workspaces
 head -n -1 workspaces | while read line
 do
   if [[ $line != *"default"* ]]

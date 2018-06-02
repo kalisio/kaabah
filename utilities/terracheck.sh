4677a08b99c61daf54a2a8401504cdf61603c6ae
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-02 20:35:10 +0200

    Updated terracheck

--- utilities/terracheck.sh
+++ utilities/terracheck.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # List the workspaces
-terraform workspace list > workspaces_list
+terraform workspace list > wspaces
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
 # Check the workspaces
-head -n -1 workspaces_list | while read workspace
+head -n -1 wspaces | while read wspace
 do
-  if [[ $workspace != *"default"* ]]
+  if [[ $wspace != *"default"* ]]
   then
-    terraform workspace select $workspace
+    terraform workspace select $wspace
     if [ $? -ne 0 ]; then
       exit 1
     fi
-    terraform apply -var-file "workspaces/$workspace.tfvars"
+    terraform apply -var-file "workspaces/$wspace.tfvars"
     if [ $? -ne 0 ]; then
       exit 1
     fi

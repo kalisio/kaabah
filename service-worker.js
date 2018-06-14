bd7cb6a7487bbefa82f34095ab0a5c81d164fe64
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-14 17:50:33 +0000

    Deploy kalisio/kaabah to github.com/kalisio/kaabah.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,10 +21,10 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "bbc6d7cf85b0a72184a72711dcda8912"
+    "revision": "d9b14687fdbb08cd8ac91671f348c7a4"
   },
   {
-    "url": "assets/css/4.styles.913d8d1f.css",
+    "url": "assets/css/5.styles.c888f2fb.css",
     "revision": "78042c0e2949ba3d095491d85abe2502"
   },
   {
@@ -48,36 +48,40 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.6b5180ce.js",
-    "revision": "4337f71a61d05ed3f2c659ac4cd746d2"
+    "url": "assets/js/0.4a8f523d.js",
+    "revision": "413892b691bc10e58d620261716be2ad"
   },
   {
-    "url": "assets/js/1.8eb554a9.js",
-    "revision": "8b04c4938205daa67c2ba45069fe1317"
+    "url": "assets/js/1.0e60d1ab.js",
+    "revision": "b1cd6a5e9cdbd2f1486c631ca9b77d9c"
   },
   {
-    "url": "assets/js/2.57a36524.js",
-    "revision": "7f9559db105919a0afb06317f578ed79"
+    "url": "assets/js/2.8dfe9c9a.js",
+    "revision": "0c709e2a586c3385626c9ec58b32d0ef"
   },
   {
-    "url": "assets/js/3.c843e869.js",
-    "revision": "c99bea750efe1f63aa07373237b142bc"
+    "url": "assets/js/3.a26e738a.js",
+    "revision": "6983c5d0f7ed43df7f2b6643b3e9c760"
   },
   {
-    "url": "assets/js/app.d326b0d0.js",
-    "revision": "ec1bd742ef4003306e4709ac3f04ff1b"
+    "url": "assets/js/4.55f36712.js",
+    "revision": "26d032bff1c5b1efef83048110594987"
+  },
+  {
+    "url": "assets/js/app.036f95da.js",
+    "revision": "bfb207a7209cbf0b11ddb1119e05198c"
   },
   {
     "url": "how-does-it-work/index.html",
-    "revision": "296f357c7e438be594ef6180fd0fb7e3"
+    "revision": "0dac813de4e22a0642f9fc3e1e78f8c4"
   },
   {
     "url": "how-to-use-it/index.html",
-    "revision": "463cbe6f99a851e34166d9a322b9e9b5"
+    "revision": "0b6e8e732092fdb20a37f9391d0a1107"
   },
   {
     "url": "index.html",
-    "revision": "31b7a679eeb39661dc980fa353ec8604"
+    "revision": "652b7c46849b3df23915680e35c387d4"
   },
   {
     "url": "kalisio-logo-256x256.png",
@@ -92,8 +96,12 @@ self.__precacheManifest = [
     "revision": "2065682e5cd8f7ac0e1db3eec6f2078e"
   },
   {
+    "url": "LICENSE.html",
+    "revision": "32c812d8ddb09713f8e66374a132aedd"
+  },
+  {
     "url": "what-is-it/index.html",
-    "revision": "823980ecd272872b2b59c9be4cebdeb8"
+    "revision": "c7cfc380bb319362848184ce97a2aecf"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();

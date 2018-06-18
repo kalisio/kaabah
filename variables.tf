9bd046710a9b13a0cd5a2cb69efd4bf61c704c15
Luc Claustres <luc.claustres@orange.fr>
2018-06-18 15:09:19 +0200

    added new instance

--- variables.tf
+++ variables.tf
@@ -27,13 +27,14 @@ variable "instance_name" {
 }
 
 variable "instance_count" {
-  default = 2
+  default = 3
 }
 
 variable "instance_domains" {
   type    = "map"
   default = {
     "1" = "dev.kalisio.xyz",
-    "2" = "test.kalisio.xyz"
+    "2" = "test.kalisio.xyz",
+    "3" = "test.kalisio.xyz"
   }
 }

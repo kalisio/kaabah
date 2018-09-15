22bd5438cceadd72bb4b12f1053a55d48ddf08e0
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-15 23:37:16 +0200

    Fixed #28

--- scripts/deploy-services.sh
+++ scripts/deploy-services.sh
@@ -7,5 +7,5 @@ EXISTING_DOCKER_NETWORK=`docker network ls | grep $DOCKER_NETWORK`
 if [ "$EXISTING_DOCKER_NETWORK" = "" ]; then
   docker network create -d overlay --attachable $DOCKER_NETWORK
 fi
-docker stack deploy -c traefik.yml -c portainer.yml -c prometheus.yml services
+docker stack deploy -c traefik.yml -c portainer.yml -c prometheus.yml -c registry.yml services
 

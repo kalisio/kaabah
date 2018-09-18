c73cd818e0a21890ca632ab2b1b1f06e9f4ca28a
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-18 18:21:46 +0200

    Fixed #25 WIP #32

--- modules/AWS/aws-volume.tf
+++ modules/AWS/aws-volume.tf
@@ -14,4 +14,19 @@ resource "aws_volume_attachment" "swarm_volume_attachement" {
   device_name = "${var.device_names[count.index % var.worker_additional_volume_count]}"
   instance_id = "${aws_instance.swarm_worker.*.id[count.index / var.worker_additional_volume_count]}"
   volume_id   = "${aws_ebs_volume.swarm_volume.*.id[count.index]}"
+  force_detach = true
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sh /tmp/mount-volume.sh ${replace(self.device_name, "s", "xv")} DATA${count.index % var.worker_additional_volume_count}",
+    ]
+
+    connection {
+      type = "ssh"
+      user = "${var.ssh_user}"
+      private_key = "${file(var.ssh_key)}"
+      host = "${aws_instance.swarm_worker.*.public_ip[count.index / var.worker_additional_volume_count]}"
+    }
+  }
 }
+

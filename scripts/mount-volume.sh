abbb7d4db16d12a9bc9b7f9c057b06572bd2f1e9
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-19 00:00:42 +0200

    Fixed #33

--- scripts/mount-volume.sh
+++ scripts/mount-volume.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 DEVICE_NAME=$1
 MOUNT_POINT=$2
+OWNER=$3
 
-mkdir -p /mnt/$MOUNT_POINT
+# Format the volume
 mkfs -t ext4 $DEVICE_NAME
 
-# Retrieve UUID
+# Retrieve UUID to mount the volume
 # The problem is that the mapping doesn't appear to be consistent. Upon reboot the drives sometimes 
 # get mapped in a different order. This means that my /etc/fstab which specifies /dev/nvme1n1 gets 
 # one drive one time and a different drive the next => so use the UUID
 VOLUME_UUID=`blkid -s UUID -o value $DEVICE_NAME`
-
-# Mount the volume
+mkdir -p /mnt/$MOUNT_POINT
 mount UUID=$VOLUME_UUID /mnt/$MOUNT_POINT
 
 # Updated the fstab
 echo UUID=$VOLUME_UUID /mnt/$MOUNT_POINT ext4 defaults,nofail 0 2 | sudo tee -a /etc/fstab
+
+# Grant the current user the ownership of the mounted volume
+if [ "$OWNER" != "" ]; then
+  chown -R $OWNER:$OWNER /mnt/$MOUNT_POINT
+fi

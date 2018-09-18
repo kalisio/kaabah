d5886c6582b7d51f08ab9c781949f28592f852da
Christophe Nouguier <christophe.nouguier@free.fr>
2018-09-19 00:08:05 +0200

    Fixed #31

--- modules/AWS/aws-main.tf
+++ modules/AWS/aws-main.tf
@@ -5,3 +5,8 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
+
+locals {
+  worker_instance_class = "${element(split(".", var.worker_instance_type), 0)}"
+  worker_use_nvme_device = "${contains(var.nvme_list, local.worker_instance_class) ? true : false}"
+}
--- modules/AWS/aws-variables.tf
+++ modules/AWS/aws-variables.tf
@@ -66,5 +66,47 @@ variable "ssh_key" {}
 variable "key_name" {}
 
 variable "device_names" {
-  default = ["/dev/sdf", "/dev/sdg", "/dev/sdh", "/dev/sdi", "/dev/sdj", "/dev/sdk", "/dev/sdl", "/dev/sdm", "/dev/sdn", "/dev/sdo"]
+  default = [
+    "/dev/sdf", 
+    "/dev/sdg", 
+    "/dev/sdh", 
+    "/dev/sdi", 
+    "/dev/sdj", 
+    "/dev/sdk", 
+    "/dev/sdl", 
+    "/dev/sdm", 
+    "/dev/sdn", 
+    "/dev/sdo"]
+}
+
+variable "standard_devices" {
+  default = [
+    "/dev/xvdf", 
+    "/dev/xvdg", 
+    "/dev/xvdh",
+    "/dev/xvdi", 
+    "/dev/xvdj",
+    "/dev/xvdk", 
+    "/dev/xvdl", 
+    "/dev/xvdm", 
+    "/dev/xvdn", 
+    "/dev/xvdo" ]
+}
+
+variable "nvme_devices" {
+ default = [
+   "/dev/nvme1n1", 
+   "/dev/nvme2n1", 
+   "/dev/nvme3n1", 
+   "/dev/nvme4n1", 
+   "/dev/nvme5n1", 
+   "/dev/nvme6n1", 
+   "/dev/nvme7n1", 
+   "/dev/nvme8n1", 
+   "/dev/nvme9n1",
+   "/dev/nvme10n1" ]
+}
+
+variable "nvme_list" {
+  default = ["c5", "c5d", "i3.metal", "m5", "m5d", "r5", "r5d", "t3", "z1d"]
 }

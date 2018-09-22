a24169d60f32bf1c6edca6325248faa37814e8f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-22 12:08:48 +0000

    Deploy kalisio/kaabah to github.com/kalisio/kaabah.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "bb2813ddc9f04bd7645a5bfbcea0287f"
+    "revision": "84eeda3312b0bf7bafdc255d5b7c04eb"
   },
   {
     "url": "assets/css/0.styles.8280e1ab.css",
@@ -48,72 +48,72 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.faa1d7a2.js",
-    "revision": "4d6d5c136931fcf80bd08d7f5a344bcb"
+    "url": "assets/js/10.9437f54d.js",
+    "revision": "261b3a64463b0960db04487283cd6ade"
   },
   {
-    "url": "assets/js/2.ef81631e.js",
-    "revision": "70942fb94c5d2ac8769b826ae17f4834"
+    "url": "assets/js/2.e69eec40.js",
+    "revision": "c9ef5eccec94be6b75165bb145114d6e"
   },
   {
-    "url": "assets/js/3.2bac8d78.js",
-    "revision": "d404a02308c055411f327513fa0b0808"
+    "url": "assets/js/3.702158b9.js",
+    "revision": "4fe101addd45ab646bc2d324179632f0"
   },
   {
-    "url": "assets/js/4.6b0f67a7.js",
-    "revision": "f3025062d37a842dd793a25253ad55ad"
+    "url": "assets/js/4.8eecaaf7.js",
+    "revision": "3eb9a3f720f502ac4a347b68a38cef1e"
   },
   {
-    "url": "assets/js/5.3932839c.js",
-    "revision": "0aafc354d479558216d10ad1e03429cc"
+    "url": "assets/js/5.1ea87e1e.js",
+    "revision": "416605432aadef8265b10290a85dc8b5"
   },
   {
-    "url": "assets/js/6.861fc3e8.js",
-    "revision": "351013ae67f803c3b01f84caeb4433e2"
+    "url": "assets/js/6.098f9386.js",
+    "revision": "254bbfb867b229a9fa7d9d953742dc1e"
   },
   {
-    "url": "assets/js/7.ee295345.js",
-    "revision": "8a1eeae1950f3593d3bbf6b8ea72ef22"
+    "url": "assets/js/7.ff976bc6.js",
+    "revision": "22a6e18838e12bb6a7a9051fe48851c2"
   },
   {
-    "url": "assets/js/8.af007864.js",
-    "revision": "e5005bd5af24fecd863f46b3b466a239"
+    "url": "assets/js/8.3b04fe92.js",
+    "revision": "24316c8f500b7e7c6084719dc467a807"
   },
   {
-    "url": "assets/js/9.a9b9a2ff.js",
-    "revision": "d7c3a4323fce14b2dd92223a8983b6e1"
+    "url": "assets/js/9.b55dbb8d.js",
+    "revision": "95a3670344578ecc9b98547c979878a2"
   },
   {
-    "url": "assets/js/app.d6885073.js",
-    "revision": "9846d8574feb61f523f164d76d7945c9"
+    "url": "assets/js/app.b82fdef6.js",
+    "revision": "3af0201f741540f68affd5978f357418"
   },
   {
     "url": "CONTRIBUTING.html",
-    "revision": "0b1c960c32f185003474a9d625385b22"
+    "revision": "cddc9a572f1526b23cd4ed6f8b066255"
   },
   {
     "url": "how-does-it-work/index.html",
-    "revision": "55f37d0278d5402adfc8888a9ee6d68b"
+    "revision": "d0412cf991ae1d301cbc9d50784b3e9b"
   },
   {
     "url": "how-to-use-it/getting-started.html",
-    "revision": "b6404772fba95fa44b397db077df3385"
+    "revision": "51b6f92f466e54687a104ba962ad68fa"
   },
   {
     "url": "how-to-use-it/tips.html",
-    "revision": "c3fc53bbe49a956d05a6432cbc9ac803"
+    "revision": "7d70211ed02534067ccc895d7da5aea0"
   },
   {
     "url": "index.html",
-    "revision": "d010248165a848db798a7913791b964f"
+    "revision": "2eb92f16fc92ecca66741623cc08f7b2"
   },
   {
     "url": "LICENSE.html",
-    "revision": "57815f8d60d1921f20899f1af80403c8"
+    "revision": "56822592ac567de7d86a1caa30012677"
   },
   {
     "url": "what-is-it/index.html",
-    "revision": "de911cc66d501bfcc5b95f94a22afec6"
+    "revision": "731851abef46045aab7c5deff97a3c2f"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();

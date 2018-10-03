10ca992a884190c2de2489c87efa04275da64d51
Christophe Nouguier <christophe.nouguier@free.fr>
2018-10-03 21:23:54 +0200

    WIP #35

---
+++ scripts/label-node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NODE_IP=$1
+LABELS=$2
+
+NODE_ID=`docker node ls -q | xargs docker node inspect -f '{{ .ID }} {{ .Status.Addr }}' | grep $NODE_IP | cut -d" " -f1`
+
+for LABEL in $LABELS; do
+  docker node update --label-add $LABEL $NODE_ID
+done

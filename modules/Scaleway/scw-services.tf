cc9faf94141e3782c2127475dfcc5acf651a5894
Christophe Nouguier <christophe.nouguier@free.fr>
2019-03-28 08:04:15 +0100

    fix: Services are not removed when tainting the resource (close #76)

--- modules/Scaleway/scw-services.tf
+++ modules/Scaleway/scw-services.tf
@@ -11,7 +11,9 @@ resource "null_resource" "services" {
 
   provisioner "remote-exec" {
     inline = [
-      "mkdir -p kaabah",
+      # we stop the services and remove the files in the case the resource is recreated 
+      "sudo docker stack rm kaabah", 
+      "rm -fr kaabah && mkdir kaabah",
     ]
   }
 
@@ -27,7 +29,7 @@ resource "null_resource" "services" {
 
   provisioner "remote-exec" {
     inline = [
-      "set -a && . ./.bash_profile && set +a",
+      "set -a && . ./.bash_profile && set +a", # required to take into account Docker environment variables
       "bash ~/.kaabah/install-services.sh ${var.domain} ${var.subdomain} ${var.ca_server} ${var.contact} ${var.auth_user} '${var.auth_password}' ${var.docker_network}",
       "cd kaabah && ./deploy-services.sh",
     ]

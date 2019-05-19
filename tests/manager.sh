d6288b89ef51f74edf63664e2e0cb7e0d6f319a2
Christophe Nouguier <christophe.nouguier@free.fr>
2019-05-20 01:22:00 +0200

    fix: k-node-exec should run the command or script as the use without sudo privilege (closed #103)

--- tests/manager.sh
+++ tests/manager.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+mkdir -p $HOME/shared
+echo "hello world" > shared/file
+
 echo "#!/bin/bash" >> /tmp/script.sh
-echo 'echo $1 >> $HOME/manager'  >> /tmp/script.sh
+echo 'mkdir -p $HOME/shared'  >> /tmp/script.sh
+echo 'sshfs ${SUDO_USER:-$USER}@$1:$HOME/shared $HOME/shared -o IdentityFile=$HOME/.ssh/ssh.pem -o allow_other -o StrictHostKeyChecking=no' >> /tmp/script.sh
 
 chmod +x /tmp/script.sh
 

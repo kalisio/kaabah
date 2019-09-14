fff23deae3a790718cc26ceb934c7e45b8f4cb2e
Christophe Nouguier <christophe.nouguier@free.fr>
2019-09-14 12:18:00 +0200

    fix: install-docker.sh script is not working anymore (closed #125)

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -2,8 +2,6 @@
 DOCKER_VERSION=$1
 
 # setup Docker repository
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 

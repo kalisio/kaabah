2fc706042c61c9503107a92fb809f43191929713
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-24 13:32:06 +0100

    fix: Cannot install libssl anymore (close #134)

--- scripts/setup-sshd.sh
+++ scripts/setup-sshd.sh
@@ -8,7 +8,7 @@ systemctl restart sshd
 
 # Install fail2ban
 # https://www.techrepublic.com/article/how-to-install-fail2ban-on-ubuntu-server-18-04/
-export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive #https://github.com/kalisio/kaabah/issues/134
 apt-get -y install fail2ban
 # Configure the SSHD service
 echo "[sshd]" > /etc/fail2ban/kaabah.conf

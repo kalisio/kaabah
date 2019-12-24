9d2ef5371eb60c213ce478598d2ce2237c067cf7
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-24 11:56:13 +0100

    wip: Cannot setup netplan anymore on OVH #135

--- modules/OVH/setup-netplan.sh
+++ modules/OVH/setup-netplan.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 FAILOVER_IP=$1
 
+# Copy the file
+cp /etc/netplan/50-cloud-init.yaml 50-cloud-init.yaml
+
 # setup the failover ip if any
 # configure netplan
 if [ "$FAILOVER_IP" != "" ]; then
-  echo "            addresses:" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-  echo "            - $FAILOVER_IP/32" | sudo tee -a /etc/netplan/50-cloud-init.yaml
+  echo "network:" > ens3.yml
+  echo "    ethernets:" >> ens3.yml
+  echo "        ens3:" >> ens3.yml
+  echo "            addresses:" >> ens3.yml
+  echo "            - $FAILOVER_IP/32" >> ens3.yml
+  yq m -i 50-cloud-init.yaml ens3.yml
+  rm ens3.yml
 fi
 
 # Setup the private ip
@@ -18,11 +26,18 @@ INTERFACE_INFO=`ip address show label $INTERFACE_NAME | grep ether`
 # Retrieve the mac address
 INTERFACE_MAC=${INTERFACE_INFO:15:17}
 # Configure netplan
-echo "        ens4:" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-echo "            dhcp4: true" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-echo "            match:" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-echo "                macaddress: $INTERFACE_MAC" | sudo tee -a /etc/netplan/50-cloud-init.yaml
-echo "            set-name: ens4" | sudo tee -a /etc/netplan/50-cloud-init.yaml
+echo "network:" > /tmp/ens4.yml
+echo "    ethernets:" >> /tmp/ens4.yml
+echo "        ens4:" >> /tmp/ens4.yml
+echo "            dhcp4: true" >> /tmp/ens4.yml
+echo "            match:" >> /tmp/ens4.yml
+echo "                macaddress: $INTERFACE_MAC" >> /tmp/ens4.yml
+echo "            set-name: ens4" >> /tmp/ens4.yml
+yq m -i 50-cloud-init.yaml ens4.yml
+rm ens4.yml
+
+# Copy the updated file
+mv 50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml 
 
 # Restart netplan
 sudo netplan apply

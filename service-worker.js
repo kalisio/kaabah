98c1be09d14658ccca64dce6134b4cd8e3e29bf4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-19 18:05:41 +0000

    Deploy kalisio/kaabah to github.com/kalisio/kaabah.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,27 +27,27 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "5265d343247d6aa64e9cb767202e8fb0"
+    "revision": "28453ca9a617650d258dacbe13c90cbc"
   },
   {
     "url": "about/contact.html",
-    "revision": "f09c4ff5a1e1bd65d2c80200492e72c4"
+    "revision": "f9cce4383fe60e279c61de3a01c89a81"
   },
   {
     "url": "about/contributing.html",
-    "revision": "5b34efedfc6e33ae1c394b923d9fe04a"
+    "revision": "2273a6e89a9e95de92dad836bb0ae455"
   },
   {
     "url": "about/introduction.html",
-    "revision": "dda8129022c24b451b795ed729ab0e4c"
+    "revision": "11bfc9cb00d1730921119f0ee3fef6db"
   },
   {
     "url": "about/license.html",
-    "revision": "0005cc62491f38a4739c34a0090a166d"
+    "revision": "47f38230c82b244a62a7d989fd9d8236"
   },
   {
     "url": "about/roadmap.html",
-    "revision": "cc5f174a7693990047c329373a2fa737"
+    "revision": "fdda756c9e26de9ba7bf326b013b6298"
   },
   {
     "url": "assets/css/0.styles.92de6ef3.css",
@@ -78,35 +78,35 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.14447477.js",
+    "url": "assets/js/10.019d5ae6.js",
     "revision": "48425bebf3a7debc0f66fd1c616967cf"
   },
   {
-    "url": "assets/js/11.626cc118.js",
+    "url": "assets/js/11.8d966988.js",
     "revision": "08ce2b5ac367a01d07884915774a1fef"
   },
   {
-    "url": "assets/js/12.51ff55ee.js",
+    "url": "assets/js/12.f1b50753.js",
     "revision": "6f8833f86ed9fb43b97b0042f153a229"
   },
   {
-    "url": "assets/js/13.a6057bb2.js",
+    "url": "assets/js/13.6c362905.js",
     "revision": "2efb0cd121aafef03f12727c691ef053"
   },
   {
-    "url": "assets/js/14.a5613aea.js",
+    "url": "assets/js/14.a9af6c22.js",
     "revision": "00e5c473a62d0599f8bf891c688f9a68"
   },
   {
-    "url": "assets/js/15.e6c5265f.js",
+    "url": "assets/js/15.b006e0d3.js",
     "revision": "2f06a586f35c1388be774949f53a3d96"
   },
   {
-    "url": "assets/js/16.73ae6423.js",
+    "url": "assets/js/16.a85ff922.js",
     "revision": "1986b7eb61c2be599cd145215cb99d15"
   },
   {
-    "url": "assets/js/17.46703b2c.js",
+    "url": "assets/js/17.050ca9aa.js",
     "revision": "efebf25e3bf857884cac3b47149e173a"
   },
   {
@@ -114,11 +114,11 @@ self.__precacheManifest = [
     "revision": "8a1c7532775555be111e13c1eb485dda"
   },
   {
-    "url": "assets/js/2.585c9caf.js",
+    "url": "assets/js/2.36fa6463.js",
     "revision": "68049b53bc7fd9e5f8ea508bb04c0e2a"
   },
   {
-    "url": "assets/js/3.ab4fdfbb.js",
+    "url": "assets/js/3.dd9f5cd8.js",
     "revision": "cdfc1917c3d508809887ea529f2b4365"
   },
   {
@@ -130,7 +130,7 @@ self.__precacheManifest = [
     "revision": "33e430d6d7ea345b10072459be1a7894"
   },
   {
-    "url": "assets/js/6.10d8e1a8.js",
+    "url": "assets/js/6.6ae4bf39.js",
     "revision": "ccef103f72ad7257ec66cdc804235f35"
   },
   {
@@ -138,44 +138,44 @@ self.__precacheManifest = [
     "revision": "cf9acab060971a1500efad4cf88a5050"
   },
   {
-    "url": "assets/js/8.41eac6ca.js",
+    "url": "assets/js/8.99b27aad.js",
     "revision": "baf84e9f39d30263d60c5e8065574f33"
   },
   {
-    "url": "assets/js/9.a1d7d71e.js",
+    "url": "assets/js/9.6009c940.js",
     "revision": "11b415bf6c10e31ed950d1179b13ee3a"
   },
   {
-    "url": "assets/js/app.e2148ad4.js",
-    "revision": "eae7ae732fd89f4b66afef4953195932"
+    "url": "assets/js/app.0f485e33.js",
+    "revision": "6a90e518b810045f745922228aac9a4f"
   },
   {
     "url": "guides/advanced-usage.html",
-    "revision": "53aee5acae33751ae0ae1fe4f94a493f"
+    "revision": "ab81e61e522564cec11381a743f9a023"
   },
   {
     "url": "guides/getting-started.html",
-    "revision": "7bbdd599be12848155a7e424d6981509"
+    "revision": "b529c3055207304969db77907df4c202"
   },
   {
     "url": "guides/understanding-kaabah.html",
-    "revision": "90ccf8ed49f4f8fa91faffb56c40eddc"
+    "revision": "e820964a611d7767258ac8258135a52b"
   },
   {
     "url": "index.html",
-    "revision": "60977d95d58363fee761e2653e3ed135"
+    "revision": "45dcffe286e4b34c84c24790a3dadb3b"
   },
   {
     "url": "reference/configuration-variables.html",
-    "revision": "e3947b67ea56b0ecbaccf8b13f3d0893"
+    "revision": "6599778d9fd39b90c35d74d88f93a821"
   },
   {
     "url": "reference/helper-commands.html",
-    "revision": "e5cbc0e719089ca0694096d925b0fd75"
+    "revision": "8809b391806137fbdd9310b549f72c17"
   },
   {
     "url": "tips/index.html",
-    "revision": "8b48664cf69c3461c257b44be8e9d9dd"
+    "revision": "d3fd6c7e4e85b8cd79195645c9d33395"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});

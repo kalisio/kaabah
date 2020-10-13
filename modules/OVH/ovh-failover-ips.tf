59d2f2fdea775ae7b57b9d33161fd1206ebab153
Robin Bourianes <robin.bourianes@frizko.com>
2020-10-13 17:31:50 +0200

    chore: disable failover ip with OVH for now.

--- modules/OVH/ovh-failover-ips.tf
+++ modules/OVH/ovh-failover-ips.tf
@@ -1,29 +1,29 @@
-resource "null_resource" "manager_failover_ips" {
-  count = var.OVH && length(var.manager_ips) > 0 ? var.manager_instance_count : 0
+# resource "null_resource" "manager_failover_ips" {
+#   count = var.OVH && length(var.manager_ips) > 0 ? var.manager_instance_count : 0
 
-  connection {
-    type                = "ssh"
-    bastion_host        = var.bastion_ip
-    bastion_user        = var.bastion_ssh_user
-    bastion_private_key = file(var.bastion_ssh_key)
-    host                = element(openstack_compute_instance_v2.manager_instances.*.access_ip_v4, count.index)
-    user                = var.ssh_user
-    private_key         = file(var.ssh_key)
-    timeout             = local.timeout
-  }
+#   connection {
+#     type                = "ssh"
+#     bastion_host        = var.bastion_ip
+#     bastion_user        = var.bastion_ssh_user
+#     bastion_private_key = file(var.bastion_ssh_key)
+#     host                = element(openstack_compute_instance_v2.manager_instances.*.network.1.fixed_ip_v4, count.index)
+#     user                = var.ssh_user
+#     private_key         = file(var.ssh_key)
+#     timeout             = local.timeout
+#   }
 
-  provisioner "file" {
-    source      = "modules/OVH/setup-failover-ip.sh"
-    destination = "${local.tmp_dir}/setup-failover-ip.sh"
-  }
+#   provisioner "file" {
+#     source      = "modules/OVH/setup-failover-ip.sh"
+#     destination = "${local.tmp_dir}/setup-failover-ip.sh"
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "bash ${local.tmp_dir}/setup-failover-ip.sh ${var.manager_ips[count.index]}"
-    ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "bash ${local.tmp_dir}/setup-failover-ip.sh ${var.manager_ips[count.index]}"
+#     ]
+#   }
 
-  depends_on = [
-    openstack_compute_instance_v2.manager_instances
-  ]
-}
+#   depends_on = [
+#     openstack_compute_instance_v2.manager_instances
+#   ]
+# }

9bf4f435203637100ab2e2f22ddcc751e6d0684e
Robin Bourianes <robin.bourianes@kalisio.com>
2021-10-13 13:51:00 +0200

    feat: make systemd store it's logs on persistent storage.

--- scripts/setup-prerequisites.sh
+++ scripts/setup-prerequisites.sh
@@ -10,3 +10,9 @@ cloud-init status --wait
 
 bash $TMP_DIR/setup-fail2ban.sh $SUBNET_CIDR
 bash $TMP_DIR/setup-gluster.sh
+
+# Make journald store it's logs on disk
+sudo mkdir /etc/systemd/journald.conf.d
+printf "# store logs on persistent storage\n# by default will use max 10%% of the file system, capped to 4G\n[Journal]\nStorage=persistent\n" | sudo tee /etc/systemd/journald.conf.d/persistent-logs.conf
+# journald restart may fail, but that's ok
+sudo sh -c "systemctl restart systemd-journald || true"
